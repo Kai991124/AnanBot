8a5e935fd829b7af5d93375082e489a8a3bd1452
chenshukai <chenshukai@cug.edu.cn>
2022-08-01 18:00:09 +0800

    Base files added.

--- src/plugins/auto_chat/__init__.py
+++ src/plugins/auto_chat/__init__.py
@@ -55,5 +55,6 @@ async def _(bot: Bot, event: GroupMessageEvent, text: str = Depends(check)):
     """自动插话"""
     nickname = list(bot.config.nickname)[0]
     # msg = await get_random_msg(event.group_id, nickname, text)
+    print('触发复制')
     msg = text
     await auto_chat.finish(msg)
