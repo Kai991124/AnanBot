17641db5525e37e7b32989abb2f623aae0c05226
chenshukai <chenshukai@cug.edu.cn>
2022-08-01 11:20:43 +0800

    Base files added.

--- src/plugins/kuakua/__init__.py
+++ src/plugins/kuakua/__init__.py
@@ -14,10 +14,10 @@ __plugin_meta__ = PluginMetadata(
 
 'matchers'
 
-hxd_kuakua = on_regex(pattern=r'(西西|老糖|青蛙|托尼|点苍|大少爷|咩人|我哥们儿)?是', permission=GROUP, priority=5, block=True)
-td_kuakua = on_regex(pattern=r'(泡芙|生发丸|千机|泡泡)?是', permission=GROUP, priority=5, block=True)
-sf_kuakua = on_regex(pattern=r'(安安|槐安安|唐一墨|师父)?是', permission=GROUP, priority=5, block=True)
-lp_kuakua = on_regex(pattern=r'(jerry|Jerry+)?是', permission=GROUP, priority=5, block=True)
+hxd_kuakua = on_regex(pattern=r'(西西|老糖|青蛙|托尼|点苍|大少爷|咩人|我哥们儿)是', permission=GROUP, priority=5, block=True)
+td_kuakua = on_regex(pattern=r'(泡芙|生发丸|千机|泡泡)是', permission=GROUP, priority=5, block=True)
+sf_kuakua = on_regex(pattern=r'(安安|槐安安|唐一墨|师父)是', permission=GROUP, priority=5, block=True)
+lp_kuakua = on_regex(pattern=r'(jerry|Jerry+)是', permission=GROUP, priority=5, block=True)
 
 
 @hxd_kuakua.handle()
@@ -26,7 +26,7 @@ async def _hxd(event: GroupMessageEvent):
     logger.info(f"<y>群{event.group_id}</y> | <g>{event.user_id}</g> | 请求夸夸")
     message = event.get_plaintext()
     print(message)
-    if (re.search(r'(西西|老糖|青蛙|托尼|点苍|大少爷|哥们儿)?是', message)):
+    if (re.search(r'(西西|老糖|青蛙|托尼|点苍|大少爷|哥们儿)是', message)):
         bro_dic = {
             '西西': '犀利奶歌、完美奶秀',
             '老糖': '冷静奶花、绝代奶毒',
@@ -37,7 +37,7 @@ async def _hxd(event: GroupMessageEvent):
             '咩人': '全能神',
             '我哥们儿': '犀利大哥'
         }
-        name = re.search(r'(西西|老糖|青蛙|托尼|点苍|大少爷|哥们儿)?是', message).group(1)
+        name = re.search(r'(西西|老糖|青蛙|托尼|点苍|大少爷|哥们儿)是', message).group(1)
         honor = bro_dic[name]
         message=f'{name}是{honor}！'
     await hxd_kuakua.finish(message)
@@ -48,7 +48,7 @@ async def _td(event: GroupMessageEvent):
     '夸夸我的好徒弟'
     logger.info(f"<y>群{event.group_id}</y> | <g>{event.user_id}</g> | 请求夸夸")
     message = event.get_plaintext()
-    name = re.search(r'(泡芙|生发丸|千机|泡泡)?是', message).group(1)
+    name = re.search(r'(泡芙|生发丸|千机|泡泡)是', message).group(1)
     message = f'{name}是主人的好徒弟，看到这条消息快去练手法！！'
     await td_kuakua.finish(message)
 
@@ -58,7 +58,7 @@ async def _sf(event: GroupMessageEvent):
     '夸夸我'
     logger.info(f"<y>群{event.group_id}</y> | <g>{event.user_id}</g> | 请求夸夸")
     message = event.get_plaintext()
-    name = re.search(r'(安安|槐安安|唐一墨|师父)?是', message).group(1)
+    name = re.search(r'(安安|槐安安|唐一墨|师父)是', message).group(1)
     message = f'{name}是我的主人，是世界上最好的主人！！！！！'
     await sf_kuakua.finish(message)
 
